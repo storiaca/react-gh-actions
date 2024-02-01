360b29962fd1eaae43e5360aa7a40a985026eb7c
Aleksandar Ristic <storiaca@gmail.com>
2024-02-01 09:04:21 +0100

    Add test coverage action

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,36 +1,37 @@
-import path from 'node:path';
-import { defineConfig, defaultExclude } from 'vitest/config';
-import configuration from './vite.config';
+import path from "node:path";
+import { defineConfig, defaultExclude } from "vitest/config";
+import configuration from "./vite.config";
 
 export default defineConfig({
   ...configuration,
   resolve: {
     alias: {
       ...configuration?.resolve?.alias,
-      test: path.resolve(__dirname, './test'),
+      test: path.resolve(__dirname, "./test"),
     },
   },
   test: {
     globals: true,
-    setupFiles: path.resolve(__dirname, 'test/setup.ts'),
-    exclude: [...defaultExclude, '**/*.svelte**'],
+    setupFiles: path.resolve(__dirname, "test/setup.ts"),
+    exclude: [...defaultExclude, "**/*.svelte**"],
     environmentMatchGlobs: [
-      ['**/*.test.tsx', 'jsdom'],
-      ['**/*.component.test.ts', 'jsdom'],
+      ["**/*.test.tsx", "jsdom"],
+      ["**/*.component.test.ts", "jsdom"],
     ],
     coverage: {
-      statements: 54.92,
+      statements: 59.79,
       thresholdAutoUpdate: true,
-      include: ['src/**/*'],
+      reporter: ["clover"],
+      include: ["src/**/*"],
       exclude: [
-        'test/**',
-        'vite.*.ts',
-        '**/*.d.ts',
-        '**/*.test.*',
-        '**/*.config.*',
-        '**/snapshot-tests/**',
-        '**/*.solution.tsx',
-        '**/coverage/**',
+        "test/**",
+        "vite.*.ts",
+        "**/*.d.ts",
+        "**/*.test.*",
+        "**/*.config.*",
+        "**/snapshot-tests/**",
+        "**/*.solution.tsx",
+        "**/coverage/**",
       ],
       all: true,
     },
